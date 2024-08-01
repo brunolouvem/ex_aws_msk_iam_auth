86fc626b66bf553ad82e48b6f95cad820ba8b923
Bruno Louvem <btlouvem@gmail.com>
2024-08-01 20:47:27 +0100

    Move SignedPayloadGenerator configs to config.exs

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,11 @@
 import Config
 
 config :ex_aws_msk_iam_auth,
-  region: "us-east-2"
+  region: "us-east-1",
+  service: "kafka-cluster",
+  version: "2020_10_22",
+  user_agent: "msk-elixir-client",
+  # 15 minutes
+  ttl: 900
 
 import_config "#{Mix.env()}.exs"
